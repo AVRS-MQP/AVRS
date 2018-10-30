74eb44f3b2eb575eb899d6f3f10b7c980614762b
AVRS <ryano647@gmail.com>
2018-10-30 19:15:46 -0400

    Fleshing out yun

--- arduino/YunComs/Console/stringConsole/stringConsole.ino
+++ arduino/YunComs/Console/stringConsole/stringConsole.ino
@@ -2,7 +2,10 @@
 
 const int ledPin = 13; // the pin that the LED is attached to
 int incomingByte;      // a variable to read incoming serial data into
-
+char car1[6] = "Tesla";
+char car2[5] = "Volt";
+char car3[5] = "Leaf";
+char carSel[6];
 
 void setup() {
   // initialize serial communication:
@@ -12,7 +15,7 @@ void setup() {
   while (!Console){
     ; // wait for Console port to connect.
   }
-  Console.println("You're connected to the vehicle");
+  Console.println("Now connected to the vehicle");
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
 }
@@ -32,8 +35,28 @@ void loop() {
       digitalWrite(ledPin, LOW);
     }
 
+    //Opens gas flap
+    if(incomingByte == 'O' || incomingByte == 'o') {
+      Console.println("Opening gas flap");
+      //TODO: vary two digital pin voltage to open the flap
+    }
+
+    //Asks for vehicle information for one of the three models 
+    //May expand later, right now only needs proof of concept to get info on demand
+    if(incomingByte == '1' || incomingByte == '2' || incomingByte == '3') {
+      Console.println("Recieving vehicle information");
+      
+      delay(200);
+      
+    }
+
   }
   delay(100);
+}
+
+
+char* vehicle_type(int sel) {
+  
 }
 
   
