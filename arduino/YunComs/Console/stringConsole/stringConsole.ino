6371c6041710567d25505232888bd4f94e7baea4
AVRS <ryano647@gmail.com>
2018-10-30 20:26:40 -0400

    Car select info and charging placeholders

--- arduino/YunComs/Console/stringConsole/stringConsole.ino
+++ arduino/YunComs/Console/stringConsole/stringConsole.ino
@@ -3,9 +3,14 @@
 const int ledPin = 13; // the pin that the LED is attached to
 int incomingByte;      // a variable to read incoming serial data into
 char car1[6] = "Tesla";
-char car2[5] = "Volt";
-char car3[5] = "Leaf";
+char car2[6] = "Volt";
+char car3[6] = "Leaf";
+char amp1[10] = "11 Amps";
+char amp2[10] = "22 Amps";
+char amp3[10] = "33 Amps";
 char carSel[6];
+char ampSel[10];
+
 
 void setup() {
   // initialize serial communication:
@@ -45,18 +50,34 @@ void loop() {
     //May expand later, right now only needs proof of concept to get info on demand
     if(incomingByte == '1' || incomingByte == '2' || incomingByte == '3') {
       Console.println("Recieving vehicle information");
-      
+      vehicle_type(incomingByte);
       delay(200);
-      
+      Console.println(carSel);
+      Console.println(ampSel);
+
     }
 
   }
   delay(100);
 }
 
+//User input selects between three models of vehicles based on which female charger we have slotted 
+void vehicle_type(char sel) {
 
-char* vehicle_type(int sel) {
-  
+  if(sel == '1') {
+    strcpy(carSel, car1);
+    strcpy(ampSel, amp1);
+  }
+  else if (sel == '2') {
+    strcpy(carSel, car2);
+    strcpy(ampSel, amp2);
+
+  }
+  else {
+    strcpy(carSel, car3);
+    strcpy(ampSel, amp3);
+  }
+
 }
 
   
