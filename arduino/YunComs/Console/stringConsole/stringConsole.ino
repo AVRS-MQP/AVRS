319e384c07a3889b15a3031aa3ffc13c4511ad4e
niko1499 <nxgamarra@gmail.com>
2018-11-06 13:23:52 -0500

    Coms compiles and writes, not read

--- arduino/YunComs/Console/stringConsole/stringConsole.ino
+++ arduino/YunComs/Console/stringConsole/stringConsole.ino
@@ -2,14 +2,26 @@
 
 const int ledPin = 13; // the pin that the LED is attached to
 int incomingByte;      // a variable to read incoming serial data into
+
+//Models of vehicles
 char car1[6] = "Tesla";
 char car2[6] = "Volt";
 char car3[6] = "Leaf";
-char amp1[10] = "11 Amps";
-char amp2[10] = "22 Amps";
-char amp3[10] = "33 Amps";
-char carSel[6];
-char ampSel[10];
+
+//Types of charging standards
+char charge1[8] = "tesla";
+char charge2[8] = "J1772";
+char charge3[8] = "CHAdeMO";
+
+//Charging levels
+int lvl1 = 1;
+int lvl2 = 2;
+int lvl3 = 3;
+
+char carSel[6]; //Holds string for which vehicle is selected
+char chargeSel[8];  //Holds string for which charger matches the vehicle
+int battPcnt; //Will hold random percentage of 'battery' charge
+int chgLvl; //Holds maximum charging level
 
 
 void setup() {
@@ -48,12 +60,14 @@ void loop() {
 
     //Asks for vehicle information for one of the three models 
     //May expand later, right now only needs proof of concept to get info on demand
-    if(incomingByte == '1' || incomingByte == '2' || incomingByte == '3') {
+    if(incomingByte == '1' || incomingByte == '2' || incomingByte == '3') { //Should later be set to push buttons or other physical input
       Console.println("Recieving vehicle information");
       vehicle_type(incomingByte);
       delay(200);
       Console.println(carSel);
-      Console.println(ampSel);
+      Console.println(chargeSel);
+      Console.println(battPcnt);
+      Console.println(chgLvl);
 
     }
 
@@ -64,20 +78,23 @@ void loop() {
 //User input selects between three models of vehicles based on which female charger we have slotted 
 void vehicle_type(char sel) {
 
-  if(sel == '1') {
+  battPcnt = random(1,90); //randomly sets the simulated vehicle's battery charge percentage
+  
+  if(sel == '1') { //Tesla
     strcpy(carSel, car1);
-    strcpy(ampSel, amp1);
+    strcpy(chargeSel, charge1);
+    chgLvl = lvl3;
   }
-  else if (sel == '2') {
+  else if (sel == '2') { //Volt
     strcpy(carSel, car2);
-    strcpy(ampSel, amp2);
+    strcpy(chargeSel, charge2);
+    chgLvl = lvl2;
 
   }
-  else {
+  else { //Leaf
     strcpy(carSel, car3);
-    strcpy(ampSel, amp3);
+    strcpy(chargeSel, charge3);
+    chgLvl = lvl2; //May also support level 3, needs confirmation
   }
 
 }
-
-  
--- src/coms/src/coms_node.cpp
+++ src/coms/src/coms_node.cpp
@@ -1,37 +1,84 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+#include <coms_msgs/vehicle.h>
+#include <coms_msgs/station.h>
+
 #include <sstream>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdio.h>
 
-int main(int argc, char **argv) {
+ros::Publisher msg_pub;
 
-ros::init(argc,argv, "Yun");
-ros::NodeHandle n;
+void vehicle_callback(const ros::TimerEvent&)
+{
+  ROS_INFO("Callback 1 triggered");
 
-system("ssh root@vehiclesim.local 'telnet localhost 6571' 2>&1 | tee coms_out.txt");
+  std::string line;
+  std::fstream myFile;
+  myFile.open ("coms_out", std::ios::in | std::ios::ate);
 
-ros::Publisher chatter_pub = n.advertise<std_msgs::String>("comsUplink", 1000);
-ros::Rate loop_rate(10);
 
-int count = 0;
+  coms_msgs::vehicle msg;
+  msg.headerstamp = ros::Time::now();
 
-//while(ros::ok()) {
+  if (myFile.is_open()) { 
+    while (getline (myFile,line)) { //reads through the opened text file line by line
+     // std::cout << line << '/n';
+	std::cout << line;
 
-	std_msgs::String msg;
-	std::stringstream ss;
-	ss << "connected to coms" << count;
-	msg.data = ss.str();
+      //std::cout values are place holder, needs to search for specific lines that are
+      //prewritten on the Yun side and then publish a packet to ROS based on that
+      if (std::cout == "Tesla") {
+	msg.model= "Tesla";
+	//std::cout << line;
+	//msg.charger_type = std::cout;
+//	std::cout << line;
+//	msg.battery_charge = std::cout;
+//	std::cout << line;
+//	msg.charge_level = std::cout;
+//
 
-	ROS_INFO("%s", msg.data.c_str());
+      }
 
-	chatter_pub.publish(msg);
-	ros::spinOnce();
-	loop_rate.sleep();
-	++count;
-//}		
+      else if (std::cout == "Volt") {
 
-//system("ssh root@vehiclesim.local 'telnet localhost 6571'");
-     
-return 0;
+      }
+
+      else if (std::cout == "Leaf") {
+
+      }
+
+    }
+
+    msg_pub.publish(msg);
+  }
 }
+
+  int main(int argc, char **argv) {
+
+    ros::init(argc,argv, "Yun");
+    ros::NodeHandle n;
+
+    system("ssh root@vehiclesim.local 'telnet localhost 6571' 2>&1 | tee coms_out.txt");
+
+    msg_pub = n.advertise<coms_msgs::vehicle>("comsUplink", 1000);
+    ros::Rate loop_rate(10);
+
+    int count = 0;
+  
+
+    ros::Timer timer1 = n.createTimer(ros::Duration(0.1), vehicle_callback);
+
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+    //}		
+
+    //system("ssh root@vehiclesim.local 'telnet localhost 6571'");
+
+    return 0;
+  }
 
