1f021b7f853c8ff0797aa632a1fae4e03ff204ea
Ryano647 <ryano647@gmail.com>
2018-11-08 11:37:14 -0500

    Coms text file to ROS topic

--- src/coms/src/coms_node.cpp
+++ src/coms/src/coms_node.cpp
@@ -12,11 +12,14 @@
 
 ros::Publisher msg_pub;
 
+int msgPend; //Number of messages waiting to be read
+bool readFlag; //New msg will not be read unless flag is low. Set low after last msg is processed
 void vehicle_callback(const ros::TimerEvent&)
 {
   ROS_INFO("Callback 1 triggered");
 
-  std::string line;
+  std::string line; //Reads each line and marks line as read
+  std::ostringstream stream; //Can hold lines from text file but doesn't mark read. Can be converted 				  //to string
   std::fstream myFile;
   myFile.open ("coms_out", std::ios::in | std::ios::ate);
 
@@ -29,26 +32,43 @@ void vehicle_callback(const ros::TimerEvent&)
      // std::cout << line << '/n';
 	std::cout << line;
 
-      //std::cout values are place holder, needs to search for specific lines that are
-      //prewritten on the Yun side and then publish a packet to ROS based on that
       if (std::cout == "Tesla") {
-	msg.model= "Tesla";
-	//std::cout << line;
-	//msg.charger_type = std::cout;
-//	std::cout << line;
-//	msg.battery_charge = std::cout;
-//	std::cout << line;
-//	msg.charge_level = std::cout;
-//
+	stream << line;
+	msg.model= stream.str();
+
+	std::cout << line;
+	stream << line;
+	msg.charger_type = stream.str();
+
+	std::cout << line;
+	msg.battery_charge = 50;
+
+	std::cout << line;
+	msg.charge_level = 3;
+	--msgPend;
 
       }
 
       else if (std::cout == "Volt") {
-
+	msg.model= "Volt";
+	std::cout << line;
+	msg.charger_type = "J1772";
+//	std::cout << line;
+//	msg.battery_charge = std::cout;
+//	std::cout << line;
+//	msg.charge_level = std::cout;
+	--msgPend;
       }
 
       else if (std::cout == "Leaf") {
-
+	msg.model= "Leaf";
+	std::cout << line;
+	msg.charger_type = "CHAdeMO";
+//	std::cout << line;
+//	msg.battery_charge = std::cout;
+//	std::cout << line;
+//	msg.charge_level = std::cout;
+	--msgPend;
       }
 
     }
@@ -63,6 +83,7 @@ void vehicle_callback(const ros::TimerEvent&)
     ros::NodeHandle n;
 
     system("ssh root@vehiclesim.local 'telnet localhost 6571' 2>&1 | tee coms_out.txt");
+    ++msgPend;	
 
     msg_pub = n.advertise<coms_msgs::vehicle>("comsUplink", 1000);
     ros::Rate loop_rate(10);
