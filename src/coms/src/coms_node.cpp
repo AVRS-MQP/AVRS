e2bb6889d178b32d056a27eb33b02f86d3cb3a30
Ryano647 <ryano647@gmail.com>
2018-11-18 15:40:59 -0500

    Test Push from Ryan's Laptop

--- src/coms/src/coms_node.cpp
+++ src/coms/src/coms_node.cpp
@@ -27,37 +27,42 @@ void vehicle_callback(const ros::TimerEvent&)
   coms_msgs::Vehicle msg;
   msg.headerstamp = ros::Time::now();
 
-  if (myFile.is_open()) { 
+  if (myFile.is_open()) {
     while (getline (myFile,line)) { //reads through the opened text file line by line
      // std::cout << line << '/n';
 	std::cout << line;
 
       if (std::cout == "Tesla") {
-	stream << line;
-	msg.model= stream.str();
+      	stream << line;
+      	msg.model= stream.str();
 
-	std::cout << line;
-	stream << line;
-	msg.charger_type = stream.str();
+      	std::cout << line;
+      	stream << line;
+      	msg.charger_type = stream.str();
 
-	std::cout << line;
-	msg.battery_charge = 50;
+      	std::cout << line;
+      	msg.battery_charge = 50;
 
-	std::cout << line;
-	msg.charge_level = 3;
-	--msgPend;
+      	std::cout << line;
+      	msg.charge_level = 3;
+      	--msgPend;
 
       }
 
       else if (std::cout == "Volt") {
-	msg.model= "Volt";
-	std::cout << line;
-	msg.charger_type = "J1772";
-//	std::cout << line;
-//	msg.battery_charge = std::cout;
-//	std::cout << line;
-//	msg.charge_level = std::cout;
-	--msgPend;
+        stream << line;
+      	msg.model= stream.str();
+
+      	std::cout << line;
+      	stream << line;
+      	msg.charger_type = stream.str();
+
+      	std::cout << line;
+      	msg.battery_charge = 50;
+
+      	std::cout << line;
+      	msg.charge_level = 2;
+      	--msgPend;
       }
 
       else if (std::cout == "Leaf") {
@@ -83,23 +88,22 @@ void vehicle_callback(const ros::TimerEvent&)
     ros::NodeHandle nh;
 
     system("ssh root@vehiclesim.local 'telnet localhost 6571' 2>&1 | tee coms_out.txt");
-    ++msgPend;	
+    ++msgPend;
 
     msg_pub = nh.advertise<coms_msgs::Vehicle>("comsUplink", 1000);
     ros::Rate loop_rate(10);
 
     int count = 0;
-  
+
 
     ros::Timer timer1 = nh.createTimer(ros::Duration(0.1), vehicle_callback);
 
     ros::spinOnce();
     loop_rate.sleep();
     ++count;
-    //}		
+    //}
 
     //system("ssh root@vehiclesim.local 'telnet localhost 6571'");
 
     return 0;
   }
-
