0aa449c357a0a137970f670e5d06d1a865ac5379
niko1499 <nxgamarra@gmail.com>
2018-11-24 18:04:52 -0500

    stop

--- src/vision_cloud_fusion/src/vision_cloud_fusion_node.cpp
+++ src/vision_cloud_fusion/src/vision_cloud_fusion_node.cpp
@@ -123,16 +123,6 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 	   hullCloud->push_back(pcl::PointXYZ(x+rad,y+dep,z-rad));//bottom +depth
 	 */
 
-
-
-
-
-	//hullCloud->push_back(pcl::PointXYZ(x+rad,y+(dep/2),z));//right +depth
-	//hullCloud->push_back(pcl::PointXYZ(x-rad,y+(dep/2),z));//left +depth
-	//hullCloud->push_back(pcl::PointXYZ(x,y+(dep/2),z+rad));//top +depth
-	//hullCloud->push_back(pcl::PointXYZ(x,y+(dep/2),z-rad));//bottom +depth
-
-
 	//--Output debug cloud
 	sensor_msgs::PointCloud2 output2;//create output container
 	pcl::PCLPointCloud2 temp_output2;//create PCLPC2
@@ -142,26 +132,22 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 	output2.header.frame_id="camera_depth_optical_frame";
 	cloud_pub2.publish(output2);
 
+//---Do Hull Crop
 	pcl::ConvexHull<pcl::PointXYZ> hull;
 	hull.setInputCloud(hullCloud);
 	hull.setDimension(3);
 	std::vector<pcl::Vertices> vertices;
 
-	//pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull (new pcl::PointCloud<pcl::PointXYZ>());
-
-
 	hull.reconstruct(*hullCloud, vertices);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cropResult(new pcl::PointCloud<pcl::PointXYZ>());
-	//-perform actual filter
+	//--perform actual filter
 	pcl::CropHull<pcl::PointXYZ> cropHull;//create the filter object
 	cropHull.setDim(3);//use a 3d hul
 	cropHull.setInputCloud(cloud);//the cloud getting cropped
 	cropHull.setHullIndices(vertices);//?
 	cropHull.setHullCloud(hullCloud);//the crop shape
 	cropHull.setCropOutside(true);//default is true, sets to remove outsid points
-	//std::vector<int> indices;
-	//cropHull.filter(indices);
 
 	cropHull.filter(*cropResult);//compute and set output
 	ROS_INFO("size cropResult");
@@ -175,15 +161,6 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 
 	output.header.frame_id="camera_depth_optical_frame";
 	cloud_pub.publish(output);
-
-	// vars for pose
-
-	float roll=0;
-	float pitch=0;
-	float yaw=0;
-
-
-
 
 	//---calculate normals
 	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
@@ -205,65 +182,11 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 
 	std::cout<< cloud_normals->size() <<std::endl;
 
-
-
 	//---GET Points for computing angles
 	pcl::PointXYZ pMin,pMax;
 	pcl::getMinMax3D (*cropResult,pMin,pMax);
-
-	//---publish normals cloud
-
-
-	sensor_msgs::PointCloud2 output3;//create output container
-	pcl::PCLPointCloud2 temp_output3;//create PCLPC2
-	pcl::toPCLPointCloud2(*cloud_normals,temp_output3);//convert from PCLXYZ to PCLPC2 must be pointer input
-	pcl_conversions::fromPCL(temp_output3,output3);//convert to ROS data type
-
-	output3.header.frame_id="camera_depth_optical_frame";
-	cloud_pub3.publish(output3);
-
-
-
-	/*
-	   lidar_utility_msgs::roadInfo msg;
-
-	   msg.headerstamp = ros::Time::now();
-	   msg.header.frame_id = "/world";
-	   msg.xMax=pMax.x;
-	   msg.xMin=pMin.x;
-	   msg.yMax=pMax.y;
-	   msg.yMin=pMin.y;
-	   msg.zMax=pMax.z;
-	   msg.zMin=pMin.z;
-
-
-
-	   sensor_msgs::PointCloud2 output;//create output container
-	   pcl::PCLPointCloud2 temp_output;//create PCLPC2
-	   pcl::toPCLPointCloud2(*cloud_p,temp_output);//convert from PCLXYZ to PCLPC2 must be pointer input
-	   pcl_conversions::fromPCL(temp_output,output);//convert to ROS data type
-	   pc2_pub.publish (output);// Publish the data.
-
-
-	 */
-	//--aditional pose calculations
-	pcl::CentroidPoint<pcl::PointXYZ> centroid;
-
-	pcl::PointCloud<pcl::PointXYZ>::iterator itt;
-	/*
-	   for(itt=cropResult->points.begin(); itt<cropResult->points.end();itt++){
-	   centroid.add(pcl::PointXYZ(cropResult->points[*itt].x,cropResult->points[*itt].y,cropResult->points[*itt].z));
-	   }
-	 */
-
-
-	//pcl::PointXYZ c1;
-	//centroid.get(c1);
-	//y=c1.y;
-
-
+	
 	//---plane seg
-
 	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
 	pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
 	// Create the segmentation object
@@ -290,7 +213,10 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 		<< coefficients->values[2] << " " 
 		<< coefficients->values[3] << std::endl;
 
-
+	// vars for pose
+	float roll=0;
+	float pitch=0;
+	float yaw=0;
 	//calc angles rpy from slopes xyz
 	// note abc not associated with angles of thier axis
 	roll=atan2(coefficients->values[1],1);
@@ -298,15 +224,13 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 	yaw=atan2(coefficients->values[0],1);//seems correct
 
 	//print all the inliers
-	/*
+	if(debugLevel>=3){
 	   std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
 	   for (size_t i = 0; i < inliers->indices.size (); ++i)
 	   std::cerr << inliers->indices[i] << "    " << cloud->points[inliers->indices[i]].x << " "
 	   << cloud->points[inliers->indices[i]].y << " "
 	   << cloud->points[inliers->indices[i]].z << std::endl;
-	 */
-
-
+	}
 
 
 	//the current dept dist
@@ -315,7 +239,7 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 	//other values tbd if set here
 	x=pMin.x+((pMax.x-pMin.x)/2);
 	z=pMin.z+((pMax.z-pMin.z)/2);
-
+	//constant modifiers to angle
 	float change = (M_PI/180)*3;
 	roll=roll+change;
 
@@ -327,10 +251,15 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 	pitch=0;
 	//pitch=0;
 	//yaw=yaw*-1;
-	//---Build Pose
-	//	roll=roll*(M_PI/180);
-	//	pitch=pitch*(M_PI/180);
-	//	yaw=yaw*(M_PI/180);
+
+
+	//---Build Pose/transform
+	//convert deg to rad
+	/*	roll=roll*(M_PI/180);
+		pitch=pitch*(M_PI/180);
+		yaw=yaw*(M_PI/180);
+*/	
+	
 	tf::Quaternion q_rot;
 	q_rot = tf::createQuaternionFromRPY(roll, pitch, yaw);//roll(x), pitch(y), yaw(z),
 	geometry_msgs::Pose poseFlap;
@@ -354,6 +283,8 @@ void computePose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
 
 }
 public:
+
+//activates when a new cloud is recived, convers it to ROS types and calls fusion with it as an argument
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
 	ROS_INFO("%s: In cloud callback",nodeName.c_str());
 
@@ -367,6 +298,7 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
 	computePose(temp_cloud);
 }
 
+//activates when CP publishes new target, saves to class private fields of class
 void vision_cb(const geometry_msgs::Pose& pose_msg){
 
 }
