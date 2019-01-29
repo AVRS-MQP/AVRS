dcb192125969d83c6c7de3708d847d2ef676f8e0
niko1499 <nxgamarra@gmail.com>
2019-01-29 16:50:59 -0500

    fixing TF node

--- src/tf_manipulator/src/tf_manipulator_node.cpp
+++ src/tf_manipulator/src/tf_manipulator_node.cpp
@@ -1,86 +1,109 @@
 #include <ros/ros.h>
+#include <stdio.h>
+#include <math.h>
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Pose.h>
 #include <tf/transform_broadcaster.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include <geometry_msgs/Vector3.h>
+
 int main(int argc, char** argv){
-  ros::init(argc, argv, "my_tf_listener");
+	ros::init(argc, argv, "my_tf_listener");
 
-  ros::NodeHandle node;
+	ros::NodeHandle node;
 
-  tf::TransformListener listener;
-geometry_msgs::Pose savedPose;
+	tf::TransformListener listener;
+	geometry_msgs::Pose savedPose;
+	bool savedFlag=false;
+	while (node.ok()){
+		tf::StampedTransform transform;
+		if(savedFlag==false){
+			try{
 
-  while (node.ok()){
-    tf::StampedTransform transform;
-    try{
-
-
-tf::Transform transform;
-      listener.lookupTransform("/base_link", "/flap_raw",  
-                               ros::Time(0), transform);
-
-	savedPose.position=transform.getOrigin();
-	savedPose.orientation=transform.getRotation();
-    }
-    catch (tf::TransformException ex){
-
-      ROS_WARN("%s",ex.what());
-	ROS_INFO("Using last pose information to publish new poses");
+				listener.lookupTransform("/base_link", "/flap_raw",  
+						ros::Time(0), transform);
 
 
-        float fixRoll =1.5707963;
- static tf::TransformBroadcaster br;
-        tf::Transform transf;
-        transf.setOrigin(tf::Vector3(x,y,z));
-        transf.setRotation(q_rot);
-        br.sendTransform(tf::StampedTransform(transf, ros::Time::now(), "camera_depth_optical_frame","flap_raw"));
-
-        float clearDist=.2;
-        tf::Transform clear;
-        tf::Quaternion q;
-        float fixRoll =1.5707963;
-        q.setRPY(0,0,yaw+fixRoll);
-        clear.setOrigin(tf::Vector3(0,0,-clearDist));
-        clear.setRotation(q);
-
-        br.sendTransform(tf::StampedTransform(clear,ros::Time::now(),"flap_raw","flap_clear"));
+				tf::Vector3 orig=transform.getOrigin();
 
 
-          tf::Transform touch;
-          q.setRPY(0,0,0);
-          touch.setOrigin(tf::Vector3(0,0,clearDist));
-          touch.setRotation(q);
+				savedPose.position.x=orig[0];
+				savedPose.position.y=orig[1];
+				savedPose.position.z=orig[2];
+
+				tf::Quaternion rot=transform.getRotation();
 
 
-          br.sendTransform(tf::StampedTransform(touch,ros::Time::now(),"flap_clear","flap_touch"));
 
-   static tf::TransformBroadcaster br;
-        tf::Transform transf;
-        transf.setOrigin(savedPose.position);
-        transf.setRotation(savedPose.orientation.roll,savedPosition.orientation.pitch,savedPose.orientation.yaw+fixRoll);
-        br.sendTransform(tf::StampedTransform(transf, ros::Time::now(), "camera_depth_optical_frame","flap_fixed"));
-
-        tf::Transform clear;
-        tf::Quaternion q;
-        q.setRPY(0,0,0);
-        clear.setOrigin(tf::Vector3(0,0,-clearDist));
-        clear.setRotation(q);
-
-        br.sendTransform(tf::StampedTransform(clear,ros::Time::now(),"flap_raw","flap_clear"));
+				quaternionTFToMsg(rot,savedPose.orientation);
 
 
-          tf::Transform touch;
-          q.setRPY(0,0,0);
-          touch.setOrigin(tf::Vector3(0,0,clearDist));
-          touch.setRotation(q);
+				//savedFlag=true;
+
+			}
+			catch (tf::TransformException ex){
+
+				ROS_WARN("%s",ex.what());
+			}
+		}else{
 
 
-          br.sendTransform(tf::StampedTransform(touch,ros::Time::now(),"flap_clear","flap_touch"));
+			int x=savedPose.position.x;
+			int y=savedPose.position.y;
+			int z=savedPose.position.z;
 
-	
+			tf::Quaternion savedQ;
+			quaternionMsgToTF(savedPose.orientation,savedQ);
+
+
+			// the tf::Quaternion has a method to acess roll pitch and yaw
+			double roll, pitch, yaw;
+			tf::Matrix3x3(savedQ).getRPY(roll, pitch, yaw);
+
+			// the found angles are written in a geometry_msgs::Vector3
+			geometry_msgs::Vector3 rpy;
+			//    rpy.x = roll;
+			//   rpy.y = pitch;
+			//  rpy.z = yaw;
+
+
+			//republish saved raw loc
+
+			float fixRoll =1.5707963;
+			static tf::TransformBroadcaster br;
+			tf::Transform transf;
+			transf.setOrigin(tf::Vector3(x,y,z));
+			transf.setRotation(savedQ);
+			br.sendTransform(tf::StampedTransform(transf, ros::Time::now(), "camera_depth_optical_frame","flap_raw"));
+
+
+
+			//publish roll fixed
+
+			tf::Transform rollFixed;
+			rollFixed.setOrigin(tf::Vector3(0,0,0));
+			tf::Quaternion q_rot = tf::createQuaternionFromRPY(roll,pitch,yaw+fixRoll);
+			rollFixed.setRotation(q_rot);
+			br.sendTransform(tf::StampedTransform(rollFixed,ros::Time::now(), "flap_raw","flap_fixed"));
+
+			//publish hinge
+
+			tf::Transform hinge;
+			hinge.setOrigin(tf::Vector3(.1,0,.1));
+			q_rot = tf::createQuaternionFromRPY(0,0,0);
+			hinge.setRotation(q_rot);
+			br.sendTransform(tf::StampedTransform(hinge,ros::Time::now(), "flap_fixed","flap_hinge"));
+
+
+
+
+
+		}
+
+
 	}
-
-
-  }
-  return 0;
+	return 0;
 };
